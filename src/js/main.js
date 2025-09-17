d7a79106e6b7c64ab928fd8c6f3882d12088126b
danielquillo <dannyq2@illinois.edu>
2025-09-17 14:25:30 -0500

    Basic layout, sticky header, base SCSS, smooth scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
-/* Your JS here. */
-console.log('Hello World!')
+/* Your JS here. */ 
+document.getElementById('year').textContent = new Date().getFullYear();
+
+document.querySelectorAll('a[data-nav]').forEach(a => {
+    a.addEventListener('click', e=> {
+        e.preventDefault();
+        const id = a.getAttribute('href');
+        document.querySelector(id)?.scrollIntoView({behavior: 'smooth', block: 'start'});
+    });
+});
